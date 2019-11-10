41ec491f53c2be4e1208c2c3b8f391598bbf8b64
Robson Rocha de Araújo <rrocha.araujo@gmail.com>
2019-11-10 06:22:34 -0300

    Initial Commit

---
+++ src/WebAppSvc.Web/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppSvc.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .ConfigureAppConfiguration((context, config) => {
+                       config.AddEnvironmentVariables(prefix: "WEBAPPSVC_");
+                   })
+                   .UseStartup<Startup>();
+    }
+}
---
+++ src/WebAppSvc.WinService/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+using WebAppSvc.Web;
+
+namespace WebAppSvc.WinService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(config =>
+            {
+                config.Service<WebAppService>(service =>
+                {
+                    service.ConstructUsing(s => new WebAppService(args));
+                    service.WhenStarted(s => s.Start());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                config.RunAsLocalSystem();
+                config.StartAutomatically();
+
+                config.SetDescription("My Web App Windows Service");
+                config.SetDisplayName("WebAppSvc");
+                config.SetServiceName("WebAppSvc");
+            });
+        }
+    }
+
+    public class WebAppService
+    {
+        private readonly string[] _args;
+
+        private Task _webAppTask;
+
+        private CancellationTokenSource _tokenSource;
+
+        public WebAppService(string[] args)
+        {
+            this._args = args;
+            _tokenSource = new CancellationTokenSource();
+        }
+
+        public void Start()
+        {
+            _webAppTask = Web.Program.CreateWebHostBuilder(_args)
+                            .Build()
+                            .RunAsync(_tokenSource.Token);
+        }
+
+        public void Stop()
+        {
+            _tokenSource.Cancel();
+            _webAppTask.Wait();
+        }
+    }
+}
